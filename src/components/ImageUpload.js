298aca7ed3b8f1acb51a5f75178ae8c58e83b89d
Dev.David <97886296+kun-lex@users.noreply.github.com>
2023-07-27 18:04:30 +0100

    update firebase version

--- src/components/ImageUpload.js
+++ src/components/ImageUpload.js
@@ -1,9 +1,11 @@
 import { Button } from "@material-ui/core";
 import { useState, useEffect } from "react";
 import { db, storage } from "../firebase";
-import { ref, uploadBytes, listAll, getDownloadURL } from "firebase/storage";
+// import { ref, uploadBytes, listAll, getDownloadURL } from "firebase/storage";
+import {ref, getDownloadURL}  from "firebase/storage";
 import { collection, serverTimestamp, addDoc} from 'firebase/firestore';
 import {v4} from 'uuid';
+import firebase from 'firebase/compat/app';
 import "firebase/firestore";
 import 'firebase/storage';
 
@@ -11,7 +13,6 @@ const ImageUpload = ({username}) => {
     const [image, setImage] = useState(null)
     const [progress, setProgress] = useState(0)
     const [caption, setCaption] = useState('')
-    const imageRef = ref(storage, "images/")
 
     const handleChange = (e) => {
         if (e.target.files[0]) {
@@ -20,46 +21,38 @@ const ImageUpload = ({username}) => {
     };
 
     const handleUpload = () => {
-        if (image == null) return;
-        const upLoadTask = ref(storage,`images/${image.name + v4()}`);
-        uploadBytes(upLoadTask, image).then(() => {
-            alert(" Upload sucessful.....");
-        })
+        const uploadTask = storage.ref(`images/${image.name}`).put(image);
+        uploadTask.on(
+            "state_changed",
+            (snapshot) => {
+                const progress = Math.round(
+                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                );
+                setProgress(progress);
+            },
+            (error) => {
+                console.log(error);
+                alert(error.message);
+            },
+            () => {
+                storage
+                    .ref("images")
+                    .child(image.name)
+                    .getDownloadURL()
+                    .then(url => {
+                        db.collection("posts").add({
+                            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+                            caption: caption,
+                            imageURL: url,
+                            userName: username
+                        })
+                    })
 
-        // upLoadTask.on( 
-        //     "state_changed",
-        //     (snapshot) => {
-        //         const progress = Math.round(
-        //             (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-        //         );
-        //         setProgress(progress);
-        //     },
-        //     (error) => {
-        //         console.log(error);
-        //         alert(error.message);
-        //     },
-        //     () => {
-                
-        //     }
-        // )
+            }
+        )
         setCaption(' ')
         setImage(null)
-    }
-    useEffect(() => {
-        listAll(imageRef).then((response) => {
-            response.items.forEach((item) => {
-                getDownloadURL(item).then((url) => {
-                    
-                    collection(db, "posts")(addDoc, {
-                        timestamp: serverTimestamp(),
-                        caption: caption,
-                        imageUrl: url,
-                        username: username,
-                    })
-                })
-            });
-        })
-    })
+    };
 
     return(
         <div>
